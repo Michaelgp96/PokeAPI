9198bd79b91f166c6bf1a8d08c7668e4ffd0aaa3
Michaelgp96 <michaelwow3@gmail.com>
2025-04-03 02:46:26 +0000

    Cambios

---
+++ src/Componentes/Detalle/index.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import './style.css'
+
+function Detalle(){
+
+    return (
+        <>
+        <h1>Detalle</h1>
+        </>
+    )
+
+}
+
+export default Detalle
